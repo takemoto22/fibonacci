16849d83db7414e99a3b20cc6142f4c5492ac3ea
takemoto22 <mtakemoto3126@gmail.com>
2020-04-23 14:16:06 +0900

    メモ化によるアルゴリズムの改善

--- app.js
+++ app.js
@@ -1,14 +1,29 @@
 'use strict';
+const memo = new Map();
+memo.set(0,0);
+memo.set(1,1);
+
 function fib(n){
+    if(memo.has(n)){
+        return memo.get(n);
+    }
+    const value = fib(n-1) + fib(n-2);
+    memo.set(n,value);
+    return value;
+
+}
+/*
+function fib1(n){
     if(n === 0){
         return 0;
     }else if(n === 1){
         return 1;
     }
-    return fib(n-1) + fib(n-2);
+    return fib1(n-1) + fib1(n-2);
 }
+*/
 
-const length = 40;
+const length = 100;
 for(let i=0; i <=length; i++){
     console.log('i= ' + i);
     console.log(fib(i));
